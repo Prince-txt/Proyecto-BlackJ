48bdb73bf87f293feb5f872c4f578634eb446cf5
Prince-txt <davidarenasmarin@gmail.com>
2026-05-19 03:47:09 -0500

    convertirfichas

--- src/App.java
+++ src/App.java
@@ -1,5 +1,42 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Prueba git hub nuevo codigo");
+
+        System.out.println("Convertidor de fichas hecho");
+
+        int fichas = convertirfichas();
     }
+
+    public static int convertirfichas(){
+
+        int fichas = 0;
+        boolean verd = false;
+
+        while (!verd) {
+            try {   
+                int dineros = Integer.parseInt(JOptionPane.showInputDialog("Ingrese dinero en dolares para jugar. -el monto minimo es de 1000"));
+
+                if (dineros <= 0) {
+                    JOptionPane.showInputDialog(null, "ingrese un valor mayor a 0");
+                }else if(dineros % 1000 != 0){
+
+                 JOptionPane.showInputDialog(null,"Solo se permiten numeros redondos de 1000 en 1000");
+
+                } else {
+
+                  
+                
+                    fichas = dineros/1000;
+                    verd = true;
+                }
+            }catch(Exception e){
+                JOptionPane.showInputDialog(null, "Entrada invalida");
+            }   
+        }
+        return fichas;
+    }
+
+    
 }
+
