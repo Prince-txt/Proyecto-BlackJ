7a51dec318bccacb32656e35b5f7a673a31b12e4
Prince-txt <davidarenasmarin@gmail.com>
2026-05-19 04:04:36 -0500

    Apuesta

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class App {
@@ -5,8 +7,59 @@ public class App {
 
         System.out.println("Convertidor de fichas hecho");
 
+        Random random = new Random();
+
         int fichas = convertirfichas();
+
+        boolean gamestart = true;
+
+        while (gamestart && fichas > 0) {
+            
+            String[][] jugador = new String[10][2];
+            String[][] Dealer = new String[10][2];
+
+            int Cjugador = 0;
+            int Cdealer = 0;
+
+            int apuesta = pedir(fichas);
+        }
+        
     }
+
+    public static int pedir(int fichas){
+        int apuesta = 0;
+        boolean verd = false;
+
+        while (!verd) {
+
+            try{
+
+                apuesta = Integer.parseInt(JOptionPane.showInputDialog("Tienes " + fichas + " fichas " + ", Ingrese su apuesta"));
+
+                if (apuesta <= 0){
+
+                    JOptionPane.showMessageDialog(null, "La apuesta debe ser mayor a 0");
+
+                }else if(apuesta > fichas){
+
+                     JOptionPane.showMessageDialog(null, "La apuesta es mayor a tu numero de fichas");
+                }else{
+                    
+                    verd = true;
+                    
+                }
+            }catch(Exception e){
+
+                JOptionPane.showMessageDialog(null, "Ingrese un dato valido");
+
+            }
+            
+        }
+
+        return apuesta;
+    }
+
+    
 
     public static int convertirfichas(){
 
@@ -25,9 +78,8 @@ public class App {
 
                 } else {
 
-                  
-                
                     fichas = dineros/1000;
+
                     verd = true;
                 }
             }catch(Exception e){
@@ -38,5 +90,6 @@ public class App {
     }
 
     
-}
 
+
+}
